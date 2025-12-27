ae75a23a0009b4d75b1aa5af06b490e8f9a27e0b
rodolfo <rodolfogarciamaeso@gmail.com>
2025-12-27 14:23:43 -0500

    Add home URL redirect to /matches/

--- predictions/urls.py
+++ predictions/urls.py
@@ -3,10 +3,12 @@ URL configuration for predictions app
 """
 
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'predictions'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/matches/', permanent=False), name='home'),
     path('matches/', views.matches_list, name='matches_list'),
 ]
